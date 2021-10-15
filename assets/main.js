308a68ccc38dc0be0c38c6825e259af3134d2f08
Alek90 <4lek.90@gmail.com>
2021-10-15 12:39:04 +0200

    Iniziato file js.
    Commentato obbiettivo in js. Copiate variabili-array e verificate in console.

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+/* Creare un carosello che all'azione "click" su specifici pulsanti esegua:
+    -cambiamento di "evidenza/selezione tra le immagini del carosello;
+
+    -riproduzione dell'immagine selezionata nella sezione #visited, con relativi testi; 
+*/
+
+
+// creazione/copiatura delle variabili array dal materiale
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+console.log(items, title, text);
+
+
